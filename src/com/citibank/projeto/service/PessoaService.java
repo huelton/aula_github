ef2c81bd8620d914baef3f4329253fbf1531d240
Huelton Santos <hueltondihon@gmail.com>
2022-02-05 13:02:55 -0300

    Ajuste feito em PessoaService novamente

--- src/com/citibank/projeto/service/PessoaService.java
+++ src/com/citibank/projeto/service/PessoaService.java
@@ -3,8 +3,12 @@ package com.citibank.projeto.service;
 public class PessoaService {
 
 	public static void main(String[] args) {
-		System.out.println("Servico de Pessoa");
+		System.out.println("Servico de Pessoa");		
+		System.out.println("Codigo implementado para PessoaService");
 		
+	}
+	
+	public static void pessoaService() {	
 		System.out.println("Codigo implementado para PessoaService");
 		
 	}

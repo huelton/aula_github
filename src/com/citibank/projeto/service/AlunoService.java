b23f6975f76b6dad5daaa4a0799c900cdac348fc
Huelton <hueltondihon@gmail.com>
2022-02-05 11:58:32 -0300

    adicionado AlunoService

---
+++ src/com/citibank/projeto/service/AlunoService.java
@@ -0,0 +1,14 @@
+package com.citibank.projeto.service;
+
+public class AlunoService {
+
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}

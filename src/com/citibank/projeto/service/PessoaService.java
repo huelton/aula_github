fa13536acd7c0ffee69cbf45beafe57459747ea0
Huelton <hueltondihon@gmail.com>
2022-02-05 12:52:18 -0300

    Adicionando dados ao PessoaService

--- src/com/citibank/projeto/service/PessoaService.java
+++ src/com/citibank/projeto/service/PessoaService.java
@@ -4,5 +4,8 @@ public class PessoaService {
 
 	public static void main(String[] args) {
 		System.out.println("Servico de Pessoa");
+		
+		System.out.println("Codigo implementado para PessoaService");
+		
 	}
 }

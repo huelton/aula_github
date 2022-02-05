9d11d470880f16aa69146c414800e1a93103c298
Huelton <hueltondihon@gmail.com>
2022-02-05 13:09:26 -0300

    Ajustando dados

--- src/com/citibank/projeto/service/PessoaService.java
+++ src/com/citibank/projeto/service/PessoaService.java
@@ -3,12 +3,10 @@ package com.citibank.projeto.service;
 public class PessoaService {
 
 	public static void main(String[] args) {
-		System.out.println("Servico de Pessoa");
 		
 		System.out.println("Codigo implementado para PessoaService");
 		
-	}
-	
+	}	
 	
 	public void fazAlgo() {
 		
